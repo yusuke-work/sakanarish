c9b23ca9f7989fe923ae601417997657913cadc6
yusuke <yusuke.work.d@gmail.com>
2021-10-18 20:43:38 +0900

    Add: userモデル作成とDB制約とバリデーション #24

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  crypted_password :string(255)
+#  email            :string(255)      not null
+#  first_name       :string(255)      not null
+#  last_name        :string(255)      not null
+#  salt             :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 FactoryBot.define do
   factory :user do
     
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  crypted_password :string(255)
+#  email            :string(255)      not null
+#  first_name       :string(255)      not null
+#  last_name        :string(255)      not null
+#  salt             :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
