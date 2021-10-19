3da0fbc5fd485b2e61618e420a461e0863675429
yusuke <yusuke.work.d@gmail.com>
2021-10-19 10:30:22 +0900

    Fix: ログイン､ログアウトのルーティング #25

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
   root to: 'static_pages#top'
+
+  get 'login', to: 'user_sessions/new'
+  post 'login', to: 'user_sessions/create'
+  destroy 'logout', to: 'user_sessions/destroy'
 
   resources :users, only: %i[new create]
 end
