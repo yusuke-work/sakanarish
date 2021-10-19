c3131cdfd97b794050407e84580349e175f389f5
yusuke <yusuke.work.d@gmail.com>
2021-10-19 14:56:45 +0900

    Add: seed_fuでnutrient_categoriesデータ投入 #11

---
+++ db/fixtures/development/nutrient_categories.rb
@@ -0,0 +1,57 @@
+NutrientCategory.seed(
+  {
+    # カロリー
+    id: 1,
+    name: 'calorie'
+  },
+  {
+    # タンパク質
+    id: 2,
+    name: 'protein'
+  },
+  {
+    # 脂質
+    id: 3,
+    name: 'lipid'
+  },
+  {
+    # 炭水化物
+    id: 4,
+    name: 'carbohydrate'
+  },
+  {
+    # カルシウム
+    id: 5,
+    name: 'calcium'
+  },
+  {
+    # 亜鉛
+    id: 6,
+    name: 'zinc'
+  },
+  {
+    # ビタミンA
+    id: 7,
+    name: 'vitamin_a'
+  },
+  {
+    # ビタミンD
+    id: 8,
+    name: 'vitamin_d'
+  },
+  {
+    # ビタミンE
+    id: 9,
+    name: 'vitamin_e'
+  },
+  {
+    # ビタミンB12
+    id: 10,
+    name: 'vitamin_b12'
+  },
+  {
+    # EPAとDHA
+    id: 11,
+    name: 'epa_dha'
+  },
+)
