0b3c86caaa1bccd6f48fd210c09215c6473e1831
yusuke <yusuke.work.d@gmail.com>
2021-10-19 11:56:48 +0900

    Add: question_evaluationsモデルとコントローラ作成とdb制約とバリデーション #10

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_024348) do
+ActiveRecord::Schema.define(version: 2021_10_19_025229) do
 
   create_table "nutrient_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "question_evaluations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "evaluation", null: false
+    t.bigint "question_id", null: false
+    t.bigint "nutrient_category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["nutrient_category_id"], name: "index_question_evaluations_on_nutrient_category_id"
+    t.index ["question_id"], name: "index_question_evaluations_on_question_id"
+    t.index ["user_id"], name: "index_question_evaluations_on_user_id"
   end
 
   create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -35,4 +47,7 @@ ActiveRecord::Schema.define(version: 2021_10_19_024348) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "question_evaluations", "nutrient_categories"
+  add_foreign_key "question_evaluations", "questions"
+  add_foreign_key "question_evaluations", "users"
 end
