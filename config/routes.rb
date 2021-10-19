398c68fafc4611aff9a191934d23b1c12a665963
yusuke <yusuke.work.d@gmail.com>
2021-10-19 10:27:43 +0900

    Add: user_sessionsコントローラ作成 #25

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
   root to: 'static_pages#top'
 
   resources :users, only: %i[new create]
