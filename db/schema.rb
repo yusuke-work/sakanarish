88f32027f993b9ea195dfba866dfe0c9e43710cd
yusuke <yusuke.work.d@gmail.com>
2021-10-20 14:41:21 +0900

    Add: fishesテーブルにimage_urlカラムを追加 #40

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_015544) do
+ActiveRecord::Schema.define(version: 2021_10_20_053834) do
 
   create_table "fish", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "rakuten_category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url", null: false
   end
 
   create_table "fish_nutrients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
