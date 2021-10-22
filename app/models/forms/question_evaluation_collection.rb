2f8f448c79d23b67369ce49ec8a6ef2284c93d68
yusuke <yusuke.work.d@gmail.com>
2021-10-22 23:41:47 +0900

    Fix: question_evaluationのフォームオブジェクト編集 #49

---
+++ app/models/forms/question_evaluation_collection.rb
@@ -0,0 +1,28 @@
+class Form::QuestionEvaluationCollection < Form::Base
+  DEFAULT_ITEM_COUNT = 11 # 作成したい登録フォームの数を指定
+  # 読み取りも書き込みもできるオブジェクトの属性を定義
+  attr_accessor :question_evaluations # インスタンス変数が抱える属性
+
+  def initialize(attributes = {})
+    # スーパークラスのinitializeメソッドを呼び出すattributesは引数
+    super attributes
+    self.question_evaluations = DEFAULT_ITEM_COUNT.times.map { QuestionEvaluation.new } unless self.question_evaluations.present?
+  end
+
+  def question_evaluations_attributes=(attributes)
+    # アンダーバーは使わない変数
+    self.question_evaluations = attributes.map { |_, v| QuestionEvaluation.new(v)}
+  end
+
+  def seve
+    # 全ての属性にseveが成功しないとロールバックさせる
+    # トランザクション
+    QuestionEvaluation.transaction do
+      self.question_evaluations.map(&:save!)
+    end
+      return true
+    rescue => end
+      return false
+  end
+
+end
