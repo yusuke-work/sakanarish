1347fdee19ef648a125aa6e1e32f0d78f3a146e0
yusuke <yusuke.work.d@gmail.com>
2021-10-22 16:42:58 +0900

    Fix: evaluationと外部キーをセットで保存できるようにした #49

--- app/controllers/question_evaluations_controller.rb
+++ app/controllers/question_evaluations_controller.rb
@@ -1,16 +1,24 @@
 class QuestionEvaluationsController < ApplicationController
   # skip_before_action :require_login, only: %i[index]
+  # newアクションに移動させるか
   def index
-    # まずは1レコードを保存できるようにする｡# evaluationのデフォルト値3
+    # まずは1レコードを保存できるようにする｡
     @question_evaluation_1 = QuestionEvaluation.new
     # binding.pry
   end
   
   def create
-    # 外部キーセット済み
-    # @question_evaluation_1 = QuestionEvaluation.new(question_id: 1, nutrient_category_id: 1, user_id: current_user.id)
+    # 外部キーセット(strong_parameter)
     @question_evaluation_1 = QuestionEvaluation.new(question_evaluation_params)
-    binding.pry
+    # binding.pry
+
+    if @question_evaluation_1.save
+      redirect_to question_evaluations_path, notice: '保存しました'
+    else
+      flash.now[:alert] = '失敗しました'
+      render :index
+    end
+
   end
   # question_evaluationのレコードには外部キー(question_idとnutrient_category_idとuser_id)がある｡
   # user_id以外をすでに紐付けたまま登録したい｡
@@ -18,6 +26,7 @@ class QuestionEvaluationsController < ApplicationController
   private
 
   def question_evaluation_params
-    params.require(:question_evaluation).permit(:evaluation)
+    # binding.pry
+    params.require(:question_evaluation).permit(:evaluation).merge(question_id: 1, nutrient_category_id: 1, user_id: current_user.id)
   end
 end
