c9cf68359a8d86cb73a1c5f0a8db1c9a2c049fd1
yusuke <yusuke.work.d@gmail.com>
2021-10-28 09:36:53 +0900

    Fix: favoriteコントローラ仮 #60

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,22 @@
 class FavoritesController < ApplicationController
   def create
+    # roleがguestは登録できなくする
     # favoritesレコードにuser_idとrecipe_idが入るとお気に入りしたことになる
     # ログインユーザーでfavoriteインスタンスに入れる
-    # current_user
+
+    # recipe.idはお気に入りぼたんのlink_toから渡す
+    # 
+
+    # binding.pry
+
+    favorite = current_user.favorites.build(recipe_id: params[:])
+    favorite.save!
+    redirect_to result_path, notice: 'お気に入り登録しました'
   end
 
   def destroy
+    favorite = current_user.favorites.find(params[:])
+    favorite.destroy!
+    redirect_to result_path, notice: 'お気に入り解除しました'
   end
 end
