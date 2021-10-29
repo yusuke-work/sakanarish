e34ca0b5e821fff42bbd6d704c61d19c72589546
yusuke <yusuke.work.d@gmail.com>
2021-10-29 22:18:01 +0900

    Fix: 未ログインとguestのアクセス制限とお気に入り機能のリファクタリング #72

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,15 @@
 class FavoritesController < ApplicationController
-  def create
-    # 同じレシピをお気に入りするとエラーになる｡viewで条件分岐するか
-    # gurestなら
-    if current_user.guest?
-      redirect_to result_path, alert: 'ログインしてください'
-    else 
-      # guest以外なら
-      favorite = current_user.favorites.build(recipe_id: params[:recipe_id])
-      favorite.save!
-      redirect_to result_path, notice: 'お気に入り登録しました'
-    end
+  before_action :check_general, only: %i[create destroy]
 
+  def create
+    recipe = Recipe.find(params[:recipe_id])
+    current_user.favorite(recipe)
+    redirect_to result_path, notice: 'お気に入り登録しました'
   end
 
   def destroy
-    # binding.pry
-      favorite = current_user.favorites.find_by(recipe_id: params[:id])
-      favorite.destroy!
-      redirect_to result_path, notice: 'お気に入り解除しました'
+    recipe = current_user.favorites.find(params[:id]).recipe
+    current_user.unfavorite(recipe)
+    redirect_to result_path, notice: 'お気に入り解除しました'
   end
 end
