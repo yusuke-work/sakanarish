03774460b000742719c04855a58337d28c72476c
yusuke <yusuke.work.d@gmail.com>
2021-11-26 01:00:34 +0900

    Remove: api

--- app/controllers/api/v1/vue_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::VueController < ApiController
-  before_action :set_emloyee, only: [:show]
-
-rescue_from ActiveRecord::RecordNotFound do |exception|
-  render json: { error: '404 not found' }, status: 404
-end
-
-
